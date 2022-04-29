e0d1e8f08fba3b308173500bcf8c24dc386d1d74
AllanGame <allangameyt28@gmail.com>
2022-04-29 18:59:05 -0500

    Add database system

--- src/index.js
+++ src/index.js
@@ -1,16 +1,50 @@
 import { Client, Intents } from 'discord.js' 
 import { TOKEN } from './constants.js';
 import parse from './parser.js';
+import path from 'path';
+import fs from 'fs';
+
+const FIVE_MINUTES = 1000 * 60 * 5;
+const __dirname = path.resolve(path.dirname(''));
 
 const client = new Client({ 
 	partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
     intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
  });
 
-const guildsData = new Map();
+let guildsData = new Map();
+
+function saveData() {
+	const data = JSON.stringify(Object.fromEntries(guildsData));
+	fs.writeFileSync(path.join(__dirname, "./data.json"), data, (error) => {
+		if(error) {
+			throw error;
+		}
+	})
+	console.log('[INFO] Data was saved.');
+}
+
 
 client.once('ready', () => {
-	console.log('Ready!');
+	console.log('[INFO] Getting data from data.json');
+	try {
+		fs.readFile(path.join(__dirname,"./data.json"), 'utf-8', (error, data) => {	
+			if(error) {
+				throw error;
+			}
+
+			const entries = Object.entries(JSON.parse(data));
+			guildsData = new Map(entries);
+			console.log(`[INFO] Recovered ${entries.length} guilds from data.json`)
+		})
+	} catch (error) {
+		console.error(
+			'[ERROR] Something went wrong trying to get data from data.json. Check if the file exists, for now the map will be empty.'
+			)
+	}
+
+	setInterval(saveData, FIVE_MINUTES)
+	console.log('[INFO] Bot is ready.');
 });
 
 client.on('interactionCreate', async (interaction) => {
